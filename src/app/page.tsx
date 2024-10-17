4dbb55bf138e55d06de996ec11c82a21a33de312
Shubham <shubham1235813@gmail.com>
2024-10-17 16:31:26 +0530

    back to initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CarouselContainer from "@/components/carousel/CarouselContainer";
 export default async function Home() {
   const maxResults = 10;
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/images?limit=${maxResults}`
+    `http://localhost:3000/api/images?limit=${maxResults}`
   );
 
   const { data }: { data: string[] } = await response.json();
